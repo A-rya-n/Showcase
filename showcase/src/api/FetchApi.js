44dcba7595627a9f14cae7a23cf05c51945b2dcb
Dev Nitro <A-rya-n>
2023-03-21 12:27:41 +0530

    working on json server

---
+++ showcase/src/api/FetchApi.js
@@ -0,0 +1,12 @@
+// const url = "http://localhost:3000";
+// const FetchApi = ({ dataHandler }) => {
+//   return fetch(url)
+//     .then((response) => {
+//       response.json();
+//     })
+//     .then((data) => {
+//       dataHandler(data);
+//     });
+// };
+
+// export default FetchApi;
--- showcase/src/body/Body.js
+++ showcase/src/body/Body.js
@@ -1,5 +1,29 @@
+import React from "react";
+import { useEffect, useState } from "react";
+// import FetchApi from "../api/FetchApi";
+
 const Body = () => {
-  return <p>Body</p>;
+  const url = "http://localhost:3000/posts";
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setInfo(data);
+      });
+  }, []);
+
+  console.log(info);
+
+  return (
+    <>
+      <p>Body</p>
+    </>
+  );
 };
 
 export default Body;
