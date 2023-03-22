e0c93968c52c2efb5035f8a1648f42683a935abd
Dev Nitro <A-rya-n>
2023-03-22 15:29:00 +0530

    worked on add/delete

--- showcase/src/body/DisplayData.js
+++ showcase/src/body/DisplayData.js
@@ -15,7 +15,8 @@ const DisplayData = ({ Data }) => {
       <Table sx={{ minWidth: 650, border: 1 }}>
         <TableHead>
           <TableRow>
-            <TableCell>Product Name</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">Product Name</TableCell>
             <TableCell align="right">Manufacuring Date</TableCell>
             <TableCell align="right">Price</TableCell>
           </TableRow>
@@ -24,8 +25,9 @@ const DisplayData = ({ Data }) => {
           {Data.map((d) => (
             <TableRow key={d.id}>
               <TableCell component="th" scope="row">
-                {d.name}
+                {d.id}
               </TableCell>
+              <TableCell align="right">{d.name}</TableCell>
               <TableCell align="right">{d.mdate}</TableCell>
               <TableCell align="right">{d.price}</TableCell>
             </TableRow>
