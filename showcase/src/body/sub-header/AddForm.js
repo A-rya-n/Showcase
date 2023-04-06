c6b37474415469bb41db44637c7a009091571ebf
Aryan Purohit <aryan31.purohit@gmail.com>
2023-04-06 22:57:55 +0530

    form validation and errors

--- showcase/src/body/sub-header/AddForm.js
+++ showcase/src/body/sub-header/AddForm.js
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { AddShop } from "../../data/Edit";
 import { Button, TextField, Box, Typography } from "@mui/material";
 
 const AddForm = (props) => {
-  const [values, setValues] = useState({
-    no: "",
-    name: "",
-    category: "",
-    mail: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    AddShop(values);
-
-    setValues({
+  const formik = useFormik({
+    initialValues: {
       no: "",
       name: "",
       category: "",
       mail: "",
-    });
-  };
+    },
+    validationSchema: Yup.object({
+      no: Yup.string()
+        .max(4, "Must be 4 characters or less")
+        .required("Required"),
+      name: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      category: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      mail: Yup.string().email("Invalid E-mail address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      AddShop(values);
+    },
+  });
 
   return (
     <Box
@@ -28,43 +35,43 @@ const AddForm = (props) => {
       sx={{ width: 500, maxWidth: "100%", "& > :not(style)": { m: 1 } }}
       noValidate
       autoComplete="off"
-      onSubmit={handleSubmit}
+      onSubmit={formik.handleSubmit}
     >
-      <Typography variant="h5">Form</Typography>
+      <Typography variant="h5">Register</Typography>
       <div>
+        <label htmlFor="no">Shop No.</label>
         <TextField
-          name="no"
-          label="Shop Number"
-          defaultValue={values.no}
-          fullWidth
-          margin="normal"
-          onChange={(e) => setValues({ ...values, no: e.target.value })}
+          id="no"
+          // name="no"
+          type="text"
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // value={formik.values.no}
+          {...formik.getFieldProps("no")}
         />
+        {formik.touched.no && formik.errors.no ? (
+          <div>{formik.errors.no}</div>
+        ) : null}
+        <label htmlFor="name">Shop Name</label>
+        <TextField id="name" type="text" {...formik.getFieldProps("name")} />
+        {formik.touched.name && formik.errors.name ? (
+          <div>{formik.errors.name}</div>
+        ) : null}
+        <label htmlFor="category">Category</label>
         <TextField
-          required={true}
-          name="name"
-          label="Shop Name"
-          defaultValue={values.name}
-          margin="normal"
-          onChange={(e) => setValues({ ...values, name: e.target.value })}
+          id="category"
+          type="text"
+          {...formik.getFieldProps("category")}
         />
-        <TextField
-          required={true}
-          name="category"
-          label="Shop Category"
-          defaultValue={values.category}
-          margin="normal"
-          onChange={(e) => setValues({ ...values, category: e.target.value })}
-        />
-        <TextField
-          required={true}
-          name="mail"
-          label="E-mail"
-          defaultValue={values.mail}
-          fullWidth
-          margin="normal"
-          onChange={(e) => setValues({ ...values, mail: e.target.value })}
-        />
+        {formik.touched.category && formik.errors.category ? (
+          <div>{formik.errors.category}</div>
+        ) : null}
+        <label htmlFor="mail">E-mail</label>
+        <TextField id="mail" type="email" {...formik.getFieldProps("mail")} />
+        {formik.touched.mail && formik.errors.mail ? (
+          <div>{formik.errors.mail}</div>
+        ) : null}
+
         <Button
           type="button"
           color="error"
