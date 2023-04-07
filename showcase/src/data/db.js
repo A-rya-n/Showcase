14517e6c999492546df5eb71c3a714c378d41ca1
A-rya-n <aryan31.purohit@gmail.com>
2023-04-07 15:25:31 +0530

    minor minor changes

--- showcase/src/data/db.js
+++
@@ -1,9 +0,0 @@
-var firstRoute = require("./shops.json");
-var secondRoute = require("./products.json");
-
-module.exports = function () {
-  return {
-    firstRoute: firstRoute,
-    secondRoute: secondRoute,
-  };
-};
