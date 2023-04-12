25c12eac4e456050604ed13f8145ef7b2c54104e
A-rya-n <aryan31.purohit@gmail.com>
2023-04-12 15:30:14 +0530

    minor adjustments

--- showcase/src/footer/Footer.js
+++ showcase/src/footer/Footer.js
@@ -5,7 +5,9 @@ import { AppBar, Box, Toolbar } from "@mui/material";
 const Footer = () => {
   return (
     <>
-      <Box sx={{ flexGrow: 1, marginBottom: 0, width: 1, bottom: 0, top: 'auto' }}>
+      <Box
+        sx={{ flexGrow: 1, marginBottom: 0, width: 1, bottom: 0, top: "auto" }}
+      >
         <AppBar position="static" style={{ backgroundColor: "#343F71" }}>
           <Toolbar></Toolbar>
         </AppBar>
